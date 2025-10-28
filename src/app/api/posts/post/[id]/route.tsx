015563a59e360814d24424da61da1f14d9367b02
Luci Lua <luci.lua81@gmail.com>
2025-10-28 15:15:39 -0300

    try to fix error

--- src/app/api/posts/post/[id]/route.tsx
+++ src/app/api/posts/post/[id]/route.tsx
@@ -1,37 +1,40 @@
-import { NextResponse } from 'next/server';
-import { revalidate } from '../../../../../utils/setRevalidade';
+import { NextResponse } from 'next/server'
+import { revalidate } from '../../../../../utils/setRevalidade'
 
 export async function GET(
-    request: Request,
-    context: { params: { id: string } }
+  request: Request,
+  { params }: { params: { id: string } }
 ) {
+  const { id } = params
 
-    const { id } = context.params;
-    
-    var api_url: string;
-    if (process.env.NODE_ENV != 'production') { api_url = 'http://localhost:3003' } else { api_url = process.env.API_URL }
-    
-    try {
-        const res = await fetch(`${api_url}/api/posts/id/${id}`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${process.env.API_TOKEN}`
-            },
-            next: {
-                revalidate: revalidate
-            },
-            
-        });
+  const api_url =
+    process.env.NODE_ENV !== 'production'
+      ? 'http://localhost:3003'
+      : process.env.API_URL
 
-        if (!res.ok) {
-            const text = await res.text();
-            return NextResponse.json({ error: 'Erro ao buscar post', details: text, more: api_url }, { status: res.status });
-        }
+  try {
+    const res = await fetch(`${api_url}/api/posts/id/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.API_TOKEN}`,
+      },
+      next: { revalidate },
+    })
 
-        const data = await res.json();
-        return NextResponse.json(data);
-
-    } catch (err) {
-        return NextResponse.json({ error: 'Erro interno do servidor', details: err }, { status: 500 });
+    if (!res.ok) {
+      const text = await res.text()
+      return NextResponse.json(
+        { error: 'Erro ao buscar post', details: text, more: api_url },
+        { status: res.status }
+      )
     }
+
+    const data = await res.json()
+    return NextResponse.json(data)
+  } catch (err) {
+    return NextResponse.json(
+      { error: 'Erro interno do servidor', details: String(err) },
+      { status: 500 }
+    )
+  }
 }
